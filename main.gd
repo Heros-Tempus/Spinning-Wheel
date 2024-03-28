49e52525fa42bc2a66b91ae0cd63c5f1c4a31715
Tim Miller <tim_j_miller@yahoo.com>
2024-03-28 15:25:40 -0400

    Removed unused code

--- main.gd
+++ main.gd
@@ -37,17 +37,6 @@ var mouse_position = Vector2i(0,0)
 
 var window_size =  Vector2i(1152,1152)
 
-var keyboard = { "a":KEY_A, "b":KEY_B, "c":KEY_C, "d":KEY_D, "e":KEY_E, "f":KEY_F, "g":KEY_G, "h":KEY_H,
-"i":KEY_I, "j":KEY_J, "k":KEY_K, "l":KEY_L, "m":KEY_M, "n":KEY_N, "o":KEY_O, "p":KEY_P, "q":KEY_Q,
-"r":KEY_R, "s":KEY_S, "t":KEY_T, "u":KEY_U, "v":KEY_V, "w":KEY_W, "x":KEY_X, "y":KEY_Y, "z":KEY_Z,
-"space":KEY_SPACE, "backtick":KEY_QUOTELEFT, "comma":KEY_COMMA, "period":KEY_PERIOD, "slash":KEY_SLASH, 
-"f1":KEY_F1, "f2":KEY_F2, "f3":KEY_F3, "f4":KEY_F4, "f5":KEY_F5, "f6":KEY_F6, "f7":KEY_F7, "f8":KEY_F8, 
-"f9":KEY_F9, "f10":KEY_F10, "f11":KEY_F11, "f12":KEY_F12, "f13":KEY_F13, "f14":KEY_F14, "f15":KEY_F15, "f16":KEY_F16, 
-"f17":KEY_F17, "f18":KEY_F18, "f19":KEY_F19, "f20":KEY_F20, "f21":KEY_F21, "f22":KEY_F22, "f23":KEY_F23, "f24":KEY_F24, 
-"1":KEY_1, "2":KEY_2, "3":KEY_3, "4":KEY_4, "5":KEY_5, "6":KEY_6, "7":KEY_7, "8":KEY_8, "9":KEY_9, "0":KEY_0, 
-"kp1":KEY_KP_1, "kp2":KEY_KP_2, "kp3":KEY_KP_3, "kp4":KEY_KP_4, "kp5":KEY_KP_5,
-"kp6":KEY_KP_6, "kp7":KEY_KP_7, "kp8":KEY_KP_8, "kp9":KEY_KP_9, "kp0":KEY_KP_0, }
-
 var position = 0
 
 var slow_down_rate = 0.99
@@ -77,6 +66,7 @@ func _ready():
 		$BoxContainer/VBoxContainer/circle.rotation_degrees = 90
 	position = $BoxContainer/VBoxContainer/circle.get_child(0).get_progress_ratio()
 
+# creates enough objects to fill the wheel
 func fill_wheel():
 	for a in 2000:
 		if a*WHEELSPACING < 1:
@@ -90,7 +80,8 @@ func fill_wheel():
 			if output_side == "left":
 				align_text_flipped(b.get_child((0)),b,text_alignment)
 
-func load_file(file): # gets all lines in a plain text file and returns as an array of strings
+# gets all lines in a plain text file and returns as an array of strings
+func load_file(file): 
 	var out = []
 	var f = FileAccess.open(file,FileAccess.READ)
 	while not f.eof_reached():
@@ -101,7 +92,8 @@ func load_file(file): # gets all lines in a plain text file and returns as an ar
 		out.pop_back()
 	return out
 	
-func remove_item(file, item): # removes a single line that from the text file that matches the given string if and only if the file has more than one line
+# removes a single line that from the text file that matches the given string if and only if the file has more than one line
+func remove_item(file, item): 
 	var lines = load_file(file)
 	if len(lines) > 1:
 		lines.remove_at(lines.find(item))
@@ -109,49 +101,42 @@ func remove_item(file, item): # removes a single line that from the text file th
 		save.store_string("\n".join(lines))
 		save.close()
 
+# reads the given file, checks for lines that match configurable settings and sets the settings accordingly
 func load_settings(file):
-	var f = FileAccess.open(file,FileAccess.READ)
-	while not f.eof_reached(): # iterate through all lines until the end of file is reached
-		var line = f.get_line()
-		var x = line.split(":")
-		match x[0]:
+	var lines = load_file(file)
+	for line in lines:
+		var split_line = line.split(":")
+		match split_line[0]:
 			"text_alignment":
-				text_alignment = x[1]
+				text_alignment = split_line[1]
 			"wheel_png":
-				wheel_png = x[1]
+				wheel_png = split_line[1]
 			"wheel_color":
-				wheel_color = to_color4(x[1])
+				wheel_color = to_color4(split_line[1])
 			"text_color":
-				text_color = to_color3(x[1])
+				text_color = to_color3(split_line[1])
 			"fade_factor":
-				fade_factor = float(x[1])
+				fade_factor = float(split_line[1])
 			"output_side":
-				output_side = x[1]
+				output_side = split_line[1]
 			"output_color_change":
-				output_color_change = to_bool(x[1])
+				output_color_change = to_bool(split_line[1])
 			"output_color":
-				output_color = to_color3(x[1])
+				output_color = to_color3(split_line[1])
 			"window_size":
-				window_size = to_Vector2i(x[1])
-			"spin_triggers":
-				#set_spin_input(x[1])
-				pass
-			"program_exit":
-				#set_quit_input(x[1])
-				pass
+				window_size = to_Vector2i(split_line[1])
 			"mute":
 				$AudioStreamPlayer.set_volume_db(-800)
 			"slowdown_rate":
-				slow_down_rate = float(x[1])
+				slow_down_rate = float(split_line[1])
 				if slow_down_rate >= 1:
 					slow_down_rate = 0.99
 				elif slow_down_rate < 0:
 					slow_down_rate = 0
 			"slowdown_time":
-				slowdown_max = int(x[1])
+				slowdown_max = int(split_line[1])
 			"delete":
-				delete_items = to_bool(x[1])
-	f.close()
+				delete_items = to_bool(split_line[1])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -202,6 +187,7 @@ func _process(delta):
 				if delete_items:
 					remove_item(item_list_path, index.get_child(0).text)
 
+# creates PathFollow2D nodes with attached labels and adds them as children to the circle node. the circle node inherits from Path2D
 func add_label(text):
 	var p = PathFollow2D.new()
 	var l = Label.new()
@@ -323,65 +309,6 @@ func to_color4(x):
 func to_Vector2i(x):
 	var v = x.split(",")
 	return Vector2i(int(v[0]), int(v[1]))
-	
-func set_spin_input(x):
-	for i in x.split(","):
-		if i == "double_right_click":
-			var spin_input = InputEventMouseButton.new()
-			spin_input.button_index = MOUSE_BUTTON_RIGHT
-			InputMap.action_add_event("spin",spin_input)
-		elif i == "double_left_click":
-			var spin_input = InputEventMouseButton.new()
-			spin_input.button_index = MOUSE_BUTTON_LEFT
-			InputMap.action_add_event("spin",spin_input)
-		elif i == "double_middle_click":
-			var spin_input = InputEventMouseButton.new()
-			spin_input.button_index = MOUSE_BUTTON_MIDDLE
-			InputMap.action_add_event("spin",spin_input)
-		else:
-			var spin_input = InputEventKey.new()
-			var inp = i.split("-")
-			if inp.size() == 1:
-				spin_input.keycode = keyboard[i]
-			else:
-				spin_input.keycode = keyboard[inp[0]]
-			if inp.has("shift"):
-				spin_input.shift_pressed = true
-			if inp.has("alt"):
-				spin_input.alt_pressed = true
-			if inp.has("ctrl"):
-				spin_input.ctrl_pressed = true
-			InputMap.action_add_event("spin",spin_input)
-
-func set_quit_input(x):
-	for i in x.split(","):
-		if i == "double_right_click":
-			var quit_input = InputEventMouseButton.new()
-			quit_input.button_index = MOUSE_BUTTON_RIGHT
-			InputMap.action_add_event("quit",quit_input)
-		elif i == "double_left_click":
-			var quit_input = InputEventMouseButton.new()
-			quit_input.button_index = MOUSE_BUTTON_LEFT
-			InputMap.action_add_event("quit",quit_input)
-		elif i == "double_middle_click":
-			var quit_input = InputEventMouseButton.new()
-			quit_input.button_index = MOUSE_BUTTON_MIDDLE
-			InputMap.action_add_event("quit",quit_input)
-		else:
-			var quit_input = InputEventKey.new()
-			var inp = i.split("-")
-			if inp.size() == 1:
-				quit_input.keycode = keyboard[i]
-			else:
-				quit_input.keycode = keyboard[inp[0]]
-			if inp.has("shift"):
-				quit_input.shift_pressed = true
-			if inp.has("alt"):
-				quit_input.alt_pressed = true
-			if inp.has("ctrl"):
-				quit_input.ctrl_pressed = true
-			InputMap.action_add_event("quit",quit_input)
-
 
 func _on_box_container_hot_key_quit():
 	get_tree().quit()
